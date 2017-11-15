21d1f2f8e27ba286e3f5bf69290692811fe37d27
Simon Ritzel <simon.ritzel@gmail.com>
2017-11-15 17:04:02 +0100

    increased STOP_CORRIDOR to 8

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -26,7 +26,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 LOOKAHEAD_WPS = 200  # Number of waypoints we will publish. You can change this number
 COMFORTABLE_DECEL = -2.0
 MAX_DECEL = -4.0
-STOP_CORRIDOR = 7
+STOP_CORRIDOR = 8
 
 
 class WaypointUpdater(object):
